449636755a64f5cf7496482965240c6f123cfbd7
cajimenez96 <carlosj_utn@hotmail.com>
2024-07-02 23:48:42 -0300

    add component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "@/components/Navbar";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar/>
+      <Contact />
     </main>
   );
 }
