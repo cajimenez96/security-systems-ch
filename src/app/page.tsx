ade4c07f768c6b42d34314c75695dfecc8bde53c
Carlos Jimenez <carlos.exejimenez@gmail.com>
2024-07-03 09:33:48 -0300

    Add styles and section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Navbar from "@/components/Navbar";
 import Contact from "@/components/Contact";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar/>
-      <Contact />
+      <div className={styles.container}>
+        <About />
+        <Contact />
+      </div>
     </main>
   );
 }
---
+++ src/components/About/index.tsx
@@ -0,0 +1,14 @@
+import Heading from "../Heading";
+import styles from "./about.module.css";
+
+const About = () => {
+  return (
+    <section>
+      <div>
+        <Heading level={1} className={styles.title}>Carlos Jimenez</Heading>
+      </div>
+    </section>
+  )
+}
+
+export default About;
