748318447a7f0952dd94b15a6898fcc93c8bc121
cajimenez96 <carlosj_utn@hotmail.com>
2024-07-03 16:39:48 -0300

    Add about section

---
+++ src/components/About/About.data.ts
@@ -0,0 +1,13 @@
+export const data = {
+  name: "Carlos Jimenez",
+  description: "Soy un desarrollador Full Stack apasionado, con un enfoque especial en el desarrollo Frontend. Mi experiencia incluye la creación de aplicaciones web dinámicas y fáciles de usar, utilizando tecnologías de vanguardia para ofrecer soluciones de alta calidad y eficiencia. A lo largo de mi carrera, he perfeccionado un conjunto diverso de habilidades al trabajar en diversas empresas, lo que me permite gestionar proyectos desde el diseño inicial hasta la implementación y el mantenimiento. Mi compromiso con el aprendizaje continuo y la mejora asegura que me mantenga actualizado con las últimas tendencias y mejores prácticas de la industria.",
+  aditional: "Intereses",
+  interests: [
+    {id: 0, name: "Powerlifting"},
+    {id: 1, name: "Fútbol"},
+    {id: 2, name: "Comer"},
+    {id: 3, name: "Video juegos"},
+    {id: 4, name: "Reuniones con amigos"},
+    {id: 5, name: "Enseñar"},
+  ]
+}
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,11 +1,34 @@
 import Heading from "../Heading";
+import { data } from "./About.data";
 import styles from "./about.module.css";
+import logo from "../../../public/assets/logo.jpg";
+import Image from "next/image";
+import Text from "../Text";
+import { Motion } from "../Motion";
 
 const About = () => {
   return (
-    <section>
-      <div>
-        <Heading level={1} className={styles.title}>Carlos Jimenez</Heading>
+    <section className={styles.container} id="about">
+      <Motion>
+        <Heading level={1} className={styles.title}>
+          {data.name}
+        </Heading>
+      </Motion>
+      <div className={styles.containerInfo}>
+        <div className={styles.description}>
+          <Motion>
+            <Text>{data.description}</Text>
+          </Motion>
+          <Motion>
+            <Heading level={6} className={styles.interestsTitle}>{data.aditional}</Heading>
+            <ul>
+              {data.interests.map(i => (
+                <li key={i.id}>{i.name}</li>
+              ))}
+            </ul>
+          </Motion>
+        </div>
+        <Image src={logo} alt={data.name} className={styles.image} />
       </div>
     </section>
   )
