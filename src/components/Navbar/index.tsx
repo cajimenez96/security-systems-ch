6a3a1c9b438685f19598f96838f166eea935ac82
cajimenez96 <carlosj_utn@hotmail.com>
2024-07-03 23:21:24 -0300

    fix navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,5 @@
 "use client"
-import { useState } from "react";
 import { links, title } from "./Navbar.data";
-import { AiOutlineMenu, AiOutlineClose  } from "react-icons/ai";
 import Image from "next/image";
 import styles from "./navbar.module.css";
 import avatar from "../../../public/assets/avatar.png";
@@ -9,9 +7,6 @@ import Link from "next/link";
 import { Motion } from "../Motion";
 
 const Navbar = () => {
-  const [nav, setNav] = useState<boolean>(false);
-
-  const openMenu = () => setNav(!nav);
   
   return (
     <Motion>
@@ -20,18 +15,16 @@ const Navbar = () => {
             <Image src={avatar} alt="Avatar" className={styles.avatar} />
             <p className={styles.name}>{title}</p>
         </Link>
-        
-        <div className={styles.menu} onClick={openMenu}>
-          {!nav ? <AiOutlineMenu fill="#ffff" className={styles.bottonMenu} /> : <AiOutlineClose fill="#ffff" className={styles.bottonMenu}/> }
-        </div>
 
-        <ul className={`${styles.list} ${nav ? styles.open : styles.close}`}>
+
+        <ul className={styles.list}>
           {links.map(element => (
-            <Link href={element.url} key={element.id}>
+            <Link href={element.url} key={element.id} className={styles.link}>
               {element.name}
             </Link>
           ))}
         </ul>
+
       </nav>
     </Motion>
   );
